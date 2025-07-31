5e3ab258fe84ed201ab3dc204e463a00b13a7fde
Chetan sharma <69357055+IAMDeveloperChetanSharma@users.noreply.github.com>
2025-07-31 20:28:02 +0530

    Update App.tsx

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,28 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Suspense } from 'react'
+import { initialWindowMetrics, SafeAreaProvider } from 'react-native-safe-area-context'
+import { Provider as StoreProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { webViewBridgeAdapter } from './features/spartacus-webview/services/webview-bridge-adapter/webview-bridge-adapter'
+import { WebViewBridgeAdapterContext } from './features/spartacus-webview/services/webview-bridge-adapter/webview-bridge-adapter-provider'
+import { NavigationContainer } from './navigation/navigation-container'
+import { persistor, store } from './services/redux/store'
+import { ThemeProvider } from './theme/components/theme-provider'
+import { AccessibilityProvider } from './utils/accessibility/components/accessibility-provider'
+import { TextStyleProvider } from './utils/accessibility/components/text-style-provider'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <StoreProvider store={store}>
+        <ThemeProvider>
+          <AccessibilityProvider>
+            <TextStyleProvider>
+              <PersistGate loading={null} persistor={persistor}>
+                <WebViewBridgeAdapterContext.Provider value={webViewBridgeAdapter}>
+                  <Suspense fallback={null}>
+                    <NavigationContainer />
+                  </Suspense>
+                </WebViewBridgeAdapterContext.Provider>
+              </PersistGate>
+            </TextStyleProvider>
+          </AccessibilityProvider>
+        </ThemeProvider>
+      </StoreProvider>
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
